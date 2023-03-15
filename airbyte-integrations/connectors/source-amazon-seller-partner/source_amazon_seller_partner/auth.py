5ef0682bbfb76de403f91b01f4a6e427945e5f1d
Guy Feldman <guy.feldman@KMBPM1-GuyFeldman.local>
2023-03-15 18:38:00 -0400

    remove leak

--- airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/auth.py
+++ airbyte-integrations/connectors/source-amazon-seller-partner/source_amazon_seller_partner/auth.py
@@ -99,7 +99,6 @@ class RefreshableBotoSession:
                 RoleSessionName=self.session_name,
                 DurationSeconds=self.session_ttl,
             ).get("Credentials")
-            print(response)
             credentials = {
                 "access_key": response.get("AccessKeyId"),
                 "secret_key": response.get("SecretAccessKey"),
